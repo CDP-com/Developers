fdf359ff954d7b061d4e3aecf6120d29e5ffbbc0
irocd <cduncan@cdp.com>
2016-01-29 17:01:45 -0500

    modify for platform folder structure changes on menus

--- InstallDevEnv.bat
+++ InstallDevEnv.bat
@@ -5,8 +5,9 @@ ECHO.
 
 rem if "%1" == "" goto USAGE
 rem ECHO "%1"
-set mpath=c:\users\public
-set pp=c:\programdata\CDP\readme.txt
+set mpath=%PUBLIC%
+SET dpath=%ProgramData%
+set pp=%dpath%\CDP\readme.txt
 
 IF NOT EXIST %1\NUL goto PATHERROR
 
@@ -26,19 +27,19 @@ REM -    Tools-3rd-party
 REM -    SnapBack *
 REM ---------------------------------------
 IF NOT EXIST Notes-Meetings\NUL mkdir Notes-Meetings
-set pp=c:\programdata\CDP\Notes-Meetings\readme.txt
+set pp=%dpath%\CDP\Notes-Meetings\readme.txt
 IF EXIST %pp% xcopy %pp% Notes-Meetings\
 
 IF NOT EXIST Shortcuts\NUL mkdir Shortcuts
-set pp=c:\programdata\CDP\Shortcuts\readme.txt
+set pp=%dpath%\CDP\Shortcuts\readme.txt
 IF EXIST %pp% xcopy %pp% Shortcuts\
 
 IF NOT EXIST Tools-for-Developers\NUL mkdir Tools-for-Developers
-set pp=c:\programdata\CDP\Tools-for-Developers\readme.txt
+set pp=%dpath%\CDP\Tools-for-Developers\readme.txt
 IF EXIST %pp% xcopy %pp% Tools-for-Developers\
 
 IF NOT EXIST Tools-3rd-party\NUL mkdir Tools-3rd-party
-set pp=c:\programdata\CDP\Notes-Tools-3rd-party\readme.txt
+set pp=%dpath%\CDP\Notes-Tools-3rd-party\readme.txt
 IF EXIST %pp% xcopy %pp% Notes-Tools-3rd-party\
 
 
@@ -51,7 +52,7 @@ REM -    APPs  *
 REM -    ExtAPPs  *
 REM -    Install
 REM -    Logs
-REM -    THEmenu  *
+REM -    Menus  *
 REM -    Quarantine
 REM -    Notifications
 REM -    Templates
@@ -61,37 +62,37 @@ REM -    Settings *
 REM ---------------------------------------
 
 IF NOT EXIST APPs\NUL mkdir APPs
-set pp=c:\programdata\CDP\APPs\readme.txt
+set pp=%dpath%\CDP\APPs\readme.txt
 IF EXIST %pp% xcopy %pp% APPs\
 IF NOT EXIST ExtAPPs\NUL mkdir ExtAPPs
-set pp=c:\programdata\CDP\ExtAPPs\readme.txt
+set pp=%dpath%\CDP\ExtAPPs\readme.txt
 IF EXIST %pp% xcopy %pp% ExtAPPs\
 IF NOT EXIST Install\NUL mkdir Install
-set pp=c:\programdata\CDP\Install\readme.txt
+set pp=%dpath%\CDP\Install\readme.txt
 IF EXIST %pp% xcopy %pp% Install\
 IF NOT EXIST Logs\NUL mkdir Logs
-set pp=c:\programdata\CDP\Logs\readme.txt
+set pp=%dpath%\CDP\Logs\readme.txt
 IF EXIST %pp% xcopy %pp% Logs\
-IF NOT EXIST THEmenu\NUL mkdir THEmenu
-set pp=c:\programdata\CDP\THEmenu\readme.txt
-IF EXIST %pp% xcopy %pp% THEmenu\
+IF NOT EXIST Menus\NUL mkdir Menus
+set pp=%dpath%\CDP\Menus\readme.txt
+IF EXIST %pp% xcopy %pp% Menus\
 IF NOT EXIST Quarantine\NUL mkdir Quarantine
-set pp=c:\programdata\CDP\Quarantine\readme.txt
+set pp=%dpath%\CDP\Quarantine\readme.txt
 IF EXIST %pp% xcopy %pp% Quarantine\
 IF NOT EXIST Notifications\NUL mkdir Notifications
-set pp=c:\programdata\CDP\Notifications\readme.txt
+set pp=%dpath%\CDP\Notifications\readme.txt
 IF EXIST %pp% xcopy %pp% Notifications\
 IF NOT EXIST Update-BITS\NUL mkdir Update-BITS
-set pp=c:\programdata\CDP\Update-BITS\readme.txt
+set pp=%dpath%\CDP\Update-BITS\readme.txt
 IF EXIST %pp% xcopy %pp% Update-BITS\
 IF NOT EXIST Platform\NUL mkdir Platform  
-set pp=c:\programdata\CDP\Platform\readme.txt
+set pp=%dpath%\CDP\Platform\readme.txt
 IF EXIST %pp% xcopy %pp% Platform\
 IF NOT EXIST Settings\NUL mkdir Settings
-set pp=c:\programdata\CDP\Settings\readme.txt
+set pp=%dpath%\CDP\Settings\readme.txt
 IF EXIST %pp% xcopy %pp% Settings\ 
 IF NOT EXIST Templates\NUL mkdir Templates
-set pp=c:\programdata\CDP\Templates\readme.txt
+set pp=%dpath%\CDP\Templates\readme.txt
 IF EXIST %pp% xcopy %pp% Templates\ 
 REM ---------------------------------------
 REM - Create Directories Under ExtApps
@@ -100,33 +101,33 @@ REM -    Tools-3rd-party
 REM ---------------------------------------
 cd ExtApps
 IF NOT EXIST Tools-CDP\NUL mkdir Tools-CDP
-set pp=c:\programdata\CDP\Tools-CDP\readme.txt
+set pp=%dpath%\CDP\Tools-CDP\readme.txt
 IF EXIST %pp% xcopy %pp% Tools-CDP\ 
 IF NOT EXIST Tools-3rd-party\NUL mkdir Tools-3rd-party
-set pp=c:\programdata\CDP\Tools-3rd-party\readme.txt
+set pp=%dpath%\CDP\Tools-3rd-party\readme.txt
 IF EXIST %pp% xcopy %pp% Tools-3rd-party\ 
 cd ..
 
 REM ---------------------------------------
-REM - Create Directories Under THEmenu
+REM - Create Directories Under Menus
 REM -    Default
-REM -    CurrentUser
-REM -    SharedUser
-REM -    CompanyUser
+REM -    MyMenu
+REM -    OurMenu
+REM -    SharedMenu
 REM ---------------------------------------
-cd THEmenu
+cd Menus
 IF NOT EXIST Default\NUL mkdir Default
-set pp=c:\programdata\CDP\Default\readme.txt
+set pp=%dpath%\CDP\Default\readme.txt
 IF EXIST %pp% xcopy %pp% Default\ 
-IF NOT EXIST CurrentUser\NUL mkdir CurrentUser
-set pp=c:\programdata\CDP\CurrentUser\readme.txt
-IF EXIST %pp% xcopy %pp% CurrentUser\ 
-IF NOT EXIST SharedUser\NUL mkdir SharedUser
-set pp=c:\programdata\CDP\SharedUser\readme.txt
-IF EXIST %pp% xcopy %pp% SharedUser\ 
-IF NOT EXIST CompanyUser\NUL mkdir CompanyUser
-set pp=c:\programdata\CDP\CompanyUser\readme.txt
-IF EXIST %pp% xcopy %pp% CompanyUser\ 
+IF NOT EXIST MyMenu\NUL mkdir MyMenu
+set pp=%dpath%\CDP\MyMenu\readme.txt
+IF EXIST %pp% xcopy %pp% MyMenu\ 
+IF NOT EXIST OurMenu\NUL mkdir OurMenu
+set pp=%dpath%\CDP\OurMenu\readme.txt
+IF EXIST %pp% xcopy %pp% OurMenu\ 
+IF NOT EXIST SharedMenu\NUL mkdir SharedMenu
+set pp=%dpath%\CDP\SharedMenu\readme.txt
+IF EXIST %pp% xcopy %pp% SharedMenu\ 
 cd ..
 REM ---------------------------------------
 REM - Create Directories Under Platform
@@ -143,13 +144,13 @@ REM -    AdminUser
 REM ---------------------------------------
 cd Settings
 IF NOT EXIST LocalMachine\NUL mkdir LocalMachine
-set pp=c:\programdata\CDP\Default\readme.txt
+set pp=%dpath%\CDP\Default\readme.txt
 IF EXIST %pp% xcopy %pp% Default\ 
 IF NOT EXIST CurrentUser\NUL mkdir CurrentUser
-set pp=c:\programdata\CDP\Default\readme.txt
+set pp=%dpath%\CDP\Default\readme.txt
 IF EXIST %pp% xcopy %pp% Default\ 
 IF NOT EXIST AdminUser\NUL mkdir AdminUser
-set pp=c:\programdata\CDP\Default\readme.txt
+set pp=%dpath%\CDP\Default\readme.txt
 IF EXIST %pp% xcopy %pp% Default\ 
 cd ..
 REM ---------------------------------------
@@ -165,16 +166,16 @@ IF NOT EXIST Common\NUL mkdir Common
 IF NOT EXIST HelloWorld\NUL mkdir HelloWorld 
 IF NOT EXIST Developers\NUL mkdir Developers
 
-IF NOT EXIST C:\ProgramData\CDP\SnapBack\Apps\NUL goto MISSINGPROD
-IF NOT EXIST C:\ProgramData\CDP\SnapBack\Apps\appTemplate\NUL goto MISSINGPROD
-IF NOT EXIST C:\ProgramData\CDP\SnapBack\Apps\Common\NUL goto MISSINGPROD
-IF NOT EXIST C:\ProgramData\CDP\SnapBack\Apps\HelloWorld\NUL goto MISSINGPROD
-IF NOT EXIST C:\ProgramData\CDP\SnapBack\Apps\Developers \NUL goto MISSINGPROD
+IF NOT EXIST %dpath%\CDP\SnapBack\Apps\NUL goto MISSINGPROD
+IF NOT EXIST %dpath%\CDP\SnapBack\Apps\appTemplate\NUL goto MISSINGPROD
+IF NOT EXIST %dpath%\CDP\SnapBack\Apps\Common\NUL goto MISSINGPROD
+IF NOT EXIST %dpath%\CDP\SnapBack\Apps\HelloWorld\NUL goto MISSINGPROD
+IF NOT EXIST %dpath%\CDP\SnapBack\Apps\Developers \NUL goto MISSINGPROD
 
-xcopy C:\ProgramData\CDP\SnapBack\Apps\appTemplate appTemplate\ /S
-xcopy C:\ProgramData\CDP\SnapBack\Apps\Common Common\ /S
-xcopy C:\ProgramData\CDP\SnapBack\Apps\HelloWorld HelloWorld\ /S
-xcopy C:\ProgramData\CDP\SnapBack\Apps\Developers Developers\ /S
+xcopy %dpath%\CDP\SnapBack\Apps\appTemplate appTemplate\ /S
+xcopy %dpath%\CDP\SnapBack\Apps\Common Common\ /S
+xcopy %dpath%\CDP\SnapBack\Apps\HelloWorld HelloWorld\ /S
+xcopy %dpath%\CDP\SnapBack\Apps\Developers Developers\ /S
 
 ECHO Created Directory Structure
 goto END
